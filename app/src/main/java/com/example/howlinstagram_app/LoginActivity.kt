c36c0081ad89c6e0cda7c98f2065fc127f4790e5
naerim <naerim1119@gmail.com>
2021-01-14 16:00:26 +0900

    [ADD] Connect with Firebase

--- app/src/main/java/com/example/howlinstagram_app/LoginActivity.kt
+++ app/src/main/java/com/example/howlinstagram_app/LoginActivity.kt
@@ -1,11 +1,66 @@
 package com.example.howlinstagram_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
+    var auth : FirebaseAuth? = null
+    lateinit var email_edittext : EditText
+    lateinit var password_edittext : EditText
+    lateinit var email_login_button : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+        email_edittext = findViewById<EditText>(R.id.email_edittext)
+        password_edittext = findViewById<EditText>(R.id.password_edittext)
+        email_login_button = findViewById<Button>(R.id.email_login_button)
+
+        // 이메일 로그인 버튼
+        email_login_button.setOnClickListener {
+            signinAndSignup()
+        }
+    }
+
+    fun signinAndSignup(){
+        auth?.createUserWithEmailAndPassword(email_edittext.toString(), password_edittext.toString())
+            ?.addOnCompleteListener {
+            task ->
+                if(task.isSuccessful){
+                    // Creating a user account
+                    moveMainPage(task.result?.user)
+                }else if(task.exception?.message.isNullOrEmpty()){
+                    // Show the error message
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+                }else{
+                    // Login if you have account
+                    signinEmail()
+                }
+        }
+    }
+    fun signinEmail(){
+        auth?.signInWithEmailAndPassword(email_edittext.toString(), password_edittext.toString())
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){
+                    // Login
+                    moveMainPage(task.result?.user)
+                }else{
+                    // Show the error message
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+    fun moveMainPage(user:FirebaseUser?){
+        if(user != null){
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
