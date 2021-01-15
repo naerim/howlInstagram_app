e53ef2eb585a7a3c34a5a52e4a92786a10c09872
naerim <naerim1119@gmail.com>
2021-01-15 23:02:56 +0900

    [ADD] Google Login

--- app/src/main/java/com/example/howlinstagram_app/LoginActivity.kt
+++ app/src/main/java/com/example/howlinstagram_app/LoginActivity.kt
@@ -6,14 +6,24 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
     var auth : FirebaseAuth? = null
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
+
     lateinit var email_edittext : EditText
     lateinit var password_edittext : EditText
     lateinit var email_login_button : Button
+    lateinit var google_sign_in_button : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +32,54 @@ class LoginActivity : AppCompatActivity() {
         email_edittext = findViewById<EditText>(R.id.email_edittext)
         password_edittext = findViewById<EditText>(R.id.password_edittext)
         email_login_button = findViewById<Button>(R.id.email_login_button)
+        google_sign_in_button = findViewById<Button>(R.id.google_sign_in_button)
 
         // 이메일 로그인 버튼
         email_login_button.setOnClickListener {
             signinAndSignup()
         }
+
+        google_sign_in_button.setOnClickListener {
+            // First step
+            googleLogin()
+        }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+    }
+
+    fun googleLogin(){
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE){
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result!!.isSuccess){
+                var account = result.signInAccount
+                // Second step
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+
+    fun firebaseAuthWithGoogle(account : GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){
+                    // Login
+                    moveMainPage(task.result?.user)
+                }else{
+                    // Show the error message
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+                }
+            }
     }
 
     fun signinAndSignup(){
