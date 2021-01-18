f3cc64f0d74e31f14525f497967f9fedb0c1b0c1
naerim <naerim1119@gmail.com>
2021-01-19 02:06:55 +0900

    [ADD] Creating the main navigation tab bar

--- app/src/main/java/com/example/howlinstagram_app/MainActivity.kt
+++ app/src/main/java/com/example/howlinstagram_app/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.howlinstagram_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.howlinstagram_app.navigation.AlarmFragment
+import com.example.howlinstagram_app.navigation.DetailViewFragment
+import com.example.howlinstagram_app.navigation.GridFragment
+import com.example.howlinstagram_app.navigation.UserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottom_navigation : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottom_navigation.setOnNavigationItemSelectedListener { it ->
+            when (it.itemId) {
+                R.id.action_home -> {
+                    val detailViewFragment = DetailViewFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_search -> {
+                    val gridFragment = GridFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment).commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_add_photo -> {
+                    true
+                }
+                R.id.action_favorite_alarm -> {
+                    val alarmFragment = AlarmFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment).commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_account -> {
+                    val userFragment = UserFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            return@setOnNavigationItemSelectedListener false
+        }
     }
 }
