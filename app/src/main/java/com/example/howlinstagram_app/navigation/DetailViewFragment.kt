f43c2340acff1c0a7e1abfe2bf87ce6d4fb4c0fc
naerim <naerim1119@gmail.com>
2021-01-29 21:23:12 +0900

    DetailViewFragment, userid 매핑

--- app/src/main/java/com/example/howlinstagram_app/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/example/howlinstagram_app/navigation/DetailViewFragment.kt
@@ -4,17 +4,68 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.howlinstagram_app.R
+import com.example.howlinstagram_app.navigation.model.ContentDTO
+import com.google.firebase.firestore.FirebaseFirestore
 
 class DetailViewFragment : Fragment(){
+    var firestore : FirebaseFirestore? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail,container,false)
+        firestore = FirebaseFirestore.getInstance()
         return view
+    }
+
+    inner class DetailViewRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+        var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
+        var contentUidList : ArrayList<String> = arrayListOf()
+
+        init {
+
+            firestore?.collection("images")?.orderBy("timestamp")?.addSnapshotListener { querySnapshot, firebaseFirestoreExce ->
+                contentDTOs.clear()
+                contentUidList.clear()
+                for(snapshot in querySnapshot!!.documents){
+                    var item = snapshot.toObject(ContentDTO::class.java)
+                    contentDTOs.add(item!!)
+                    contentUidList.add(snapshot.id)
+                }
+                // 값이 새로고침 되도
+                notifyDataSetChanged()
+            }
+        }
+
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(p0.context).inflate(R.layout.item_detail, p0, false)
+            return CustomViewHolder(view)
+        }
+
+        inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+        override fun getItemCount(): Int {
+            return contentDTOs.size
+        }
+
+        override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
+            var viewholder = (p0 as CustomViewHolder).itemView
+            var detailviewitem_profile_textview : TextView
+
+            // UserId
+            detailviewitem_profile_textview = viewholder.findViewById<TextView>(R.id.detailviewitem_profile_textview)
+            detailviewitem_profile_textview.text = contentDTOs!![p1].userId
+
+
+
+        }
+
     }
 }
 
